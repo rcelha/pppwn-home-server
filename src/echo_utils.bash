532e0e8303c263005835afff929679aed0ec2b5c
Rodrigo Corrêa <rcelha@gmail.com>
2024-07-05 17:13:53 +0100

    fix: Improve retries and detection

--- src/echo_utils.bash
+++ src/echo_utils.bash
@@ -7,3 +7,29 @@ echo::ok() {
 echo::nok() {
     echo -e "\033[31m$@\033[0m"
 }
+
+echo::debug() {
+    local LINENO=${BASH_LINENO[0]:-""}
+    local SOURCE=${BASH_SOURCE[1]:-""}
+    local PREFIX=""
+
+    if [[ $LINENO  != "" && $SOURCE != "" ]]
+    then
+        PREFIX="${SOURCE}:${LINENO}: "
+    fi
+    echo -e "\033[2;37m$PREFIX$@\033[0m"
+}
+
+echo::_color_table() {
+    local text=${@:-SAMPLE}
+    for i in {30..40}
+    do
+        echo -n "| "
+        for j in {0..3}
+        do
+            echo -n "033[${j};${i}m="
+            echo -n -e "\033[${j};${i}m$text\033[0m | "
+        done
+        echo
+    done
+}
--- src/process_utils.bash
+++ src/process_utils.bash
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+source ./echo_utils.bash
+
 wait_cmd() {
     coproc read -t 10 && wait "$!" || true
+}
+
+proc::retry() {
+  local function=$1
+  local c=${2:-3}
+  local timeout=${3:-10}
+  local message=${4:-"'$function' failed"}
+
+  for i in $(seq 0 $c)
+  do
+    if $function
+    then
+      return 0
+    else
+      echo::debug $message
+      echo::debug "Wait $timeout seconds before the next retry (${i} of ${c})"
+      sleep $timeout
+    fi
+  done
+
+  echo::nok "Error: '$function' has failed too many times"
+  echo::nok "Error: $message"
+  return 1
 }
